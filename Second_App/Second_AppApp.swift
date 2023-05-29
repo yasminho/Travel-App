badadccd31aa93d4a9fcc887e7479d72fdb354b8
yasminho <yasminho@berkeley.edu>
2023-05-29 03:09:42 -0700

    Initial Commit

---
+++ Second_App/Second_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_AppApp.swift
+//  Second_App
+//
+//  Created by Yasmin Ho on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct Second_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
